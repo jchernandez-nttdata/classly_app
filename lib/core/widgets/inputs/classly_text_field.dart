5e483c160a25ffc89c598bcd77302466fae335a9
Juan Carlos Hernandez <jherncas@emeal.nttdata.com>
2026-05-16 23:57:39 -0500

    feat: Refactor ClasslyTextField to StatefulWidget for password visibility toggle

--- lib/core/widgets/inputs/classly_text_field.dart
+++ lib/core/widgets/inputs/classly_text_field.dart
@@ -1,24 +1,56 @@
 import 'package:flutter/material.dart';
 
-class ClasslyTextField extends StatelessWidget {
+class ClasslyTextField extends StatefulWidget {
   const ClasslyTextField({
     super.key,
     this.onChanged,
     this.hintText,
     this.keyboardType,
+    this.obscureText = false,
   });
 
   final void Function(String value)? onChanged;
   final String? hintText;
   final TextInputType? keyboardType;
+  final bool obscureText;
+
+  @override
+  State<ClasslyTextField> createState() => _ClasslyTextFieldState();
+}
+
+class _ClasslyTextFieldState extends State<ClasslyTextField> {
+  late bool _isObscured;
+
+  @override
+  void initState() {
+    super.initState();
+    _isObscured = widget.obscureText;
+  }
 
   @override
   Widget build(BuildContext context) {
+    final isPasswordField = widget.obscureText;
+
     return TextFormField(
-      onChanged: onChanged,
-      keyboardType: keyboardType,
+      onChanged: widget.onChanged,
+      keyboardType: widget.keyboardType,
+      obscureText: _isObscured,
       decoration: InputDecoration(
-        hintText: hintText,
+        hintText: widget.hintText,
+        suffixIcon: isPasswordField
+            ? IconButton(
+                onPressed: () {
+                  setState(() {
+                    _isObscured = !_isObscured;
+                  });
+                },
+                icon: Icon(
+                  _isObscured
+                      ? Icons.visibility_off_outlined
+                      : Icons.visibility_outlined,
+                ),
+              )
+            : null,
       ),
     );
   }
