3d7a7c0595252aeaf745138c57f80acdf2660cca
Juan Carlos Hernandez <jherncas@emeal.nttdata.com>
2026-05-19 19:37:37 -0500

    feat: Add loader display and hide functionality in BuildContext extension

--- lib/core/utils/extensions/build_context_extension.dart
+++ lib/core/utils/extensions/build_context_extension.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:classly_app/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 
@@ -5,4 +7,33 @@ extension BuildContextExtension on BuildContext {
   AppLocalizations get localizations => AppLocalizations.of(this)!;
   ColorScheme get colorScheme => Theme.of(this).colorScheme;
   TextTheme get textTheme => Theme.of(this).textTheme;
+
+  void showLoader() {
+    if (!mounted) return;
+
+    unawaited(
+      showDialog<void>(
+        context: this,
+        barrierDismissible: false,
+        builder: (_) {
+          return const PopScope(
+            canPop: false,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  void hideLoader() {
+    if (!mounted) return;
+
+    final navigator = Navigator.of(this, rootNavigator: true);
+
+    if (navigator.canPop()) {
+      navigator.pop();
+    }
+  }
 }
